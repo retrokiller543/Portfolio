f154729ae260743a8029d5d98bdb138afa84072d
Emil Schütt <emil.schutt@gmail.com>
2020-05-24 12:04:14 +0200

    Made slide animation on navlinks

--- js/jqTest.js
+++ js/jqTest.js
@@ -1,28 +1,31 @@
 $(document).ready(function () {
-    $('#ham-container').click(function (e) { 
-        e.preventDefault();
-        $('#link-container').fadeToggle();
-        $('.header-title').toggleClass('text-change');
-        $('nav').toggleClass('text-change');
-    });
-    $('.fa-react').hover(function () {
-            // over
-            $('.react-text').fadeToggle();
-        }
-    );
-    $('.fa-node').hover(function () {
-        // over
-        $('.node-text').fadeToggle();
-    }
-    );
-    $('.fa-python').hover(function () {
-        // over
-        $('.python-text').fadeToggle();
-    }
-    );
-    $('.fa-js-square').hover(function () {
-        // over
-        $('.js-text').fadeToggle();
-    }
-    );
+  // Controll the navbar
+  $("#ham-container").click(function (e) {
+    e.preventDefault();
+    // $('#link-container').fadeToggle();
+    $("#link-container").toggleClass("slide-out");
+    $("#link-container").toggleClass("slide-in");
+      $('.fa-layer-group').toggleClass('text-change')
+      $('.navbar-text').toggleClass('text-change')
+      $('.box-1').toggleClass('bar-change')
+      $('.box-2').toggleClass('bar-change')
+      $('.box-3').toggleClass('bar-change')
+  });
+  // Hover over rect, nodeJS, python, and JS logos
+  $(".fa-react").hover(function () {
+    // over
+    $(".react-text").fadeToggle();
+  });
+  $(".fa-node").hover(function () {
+    // over
+    $(".node-text").fadeToggle();
+  });
+  $(".fa-python").hover(function () {
+    // over
+    $(".python-text").fadeToggle();
+  });
+  $(".fa-js-square").hover(function () {
+    // over
+    $(".js-text").fadeToggle();
+  });
 });
